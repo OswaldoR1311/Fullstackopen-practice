57f7f5adbf16d3b80a76c3e5fe199d722dba79cf
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-22 13:19:59 -0400

    adding practice of the part 3

---
+++ node-express-part3/index.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+  {
+    content: 'hola',
+    id: 1,
+  },
+  {
+    content: 'hola1',
+    id: 2,
+  },
+  {
+    content: 'hola2',
+    id: 3,
+  },
+]
+
+const generateID = () => {
+  const maxID = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
+  return String(maxID + 1)
+}
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello world</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ error: 'Content missing' })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateID(),
+  }
+
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
