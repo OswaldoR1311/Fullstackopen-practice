db95218870557756cff854727ddb39ca2acdda8c
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-05-25 13:46:24 -0400

    Fixing root directory

--- node-express-part3/index.js
+++ node-express-part3/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const { requestLogger, unknownEndpoint } = require('./middlewares/middlewares')
 const app = express()
+const cors = require('cors')
 
 app.use(express.json())
+app.use(cors())
 
 app.use(requestLogger)
 
@@ -53,7 +55,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
