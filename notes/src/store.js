00169874d86a913980fdab97b4b32d903b75a7b3
OswaldoR1311 <oswaldorodriguez131192@gmail.com>
2026-07-13 13:49:59 -0400

    adding middleware

--- notes/src/store.js
+++ notes/src/store.js
@@ -1,57 +1,49 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 import noteService from './services/notes'
 
-// const initialNotes = [
-// 	{
-// 		id: 1,
-// 		content: 'Zustand is less complex than Redux',
-// 		important: true,
-// 	},
-// 	{
-// 		id: 2,
-// 		content: 'React app benefits from custom hooks',
-// 		important: false,
-// 	},
-// 	{
-// 		id: 3,
-// 		content: 'Remember to sleep well',
-// 		important: true,
-// 	},
-// ]
+const logger = (config) => (set, get) =>
+	config((...args) => {
+		console.log('prev state', get())
+		set(...args)
+		console.log('next state', get())
+	}, get)
 
-const useNoteStore = create((set, get) => ({
-	// notes: initialNotes,
-	notes: [],
-	filter: 'all',
-	actions: {
-		// add: (note) => set((state) => ({ notes: state.notes.concat(note) })),
-		add: async (content) => {
-			const newNote = await noteService.createNew(content)
-			set((state) => ({ notes: [...state.notes, newNote] }))
+const useNoteStore = create(
+	devtools((set, get) => ({
+		// notes: initialNotes,
+		notes: [],
+		filter: 'all',
+		actions: {
+			// add: (note) => set((state) => ({ notes: state.notes.concat(note) })),
+			add: async (content) => {
+				const newNote = await noteService.createNew(content)
+				set((state) => ({ notes: [...state.notes, newNote] }))
+			},
+			// toggleImportance: (id) =>
+			// 	set((state) => ({
+			// 		notes: state.notes.map((note) =>
+			// 			note.id === id ? { ...note, important: !note.important } : note,
+			// 		),
+			// 	})),
+			toggleImportance: async (id) => {
+				const note = get().notes.find((n) => n.id === id)
+				const updated = await noteService.update(id, {
+					...note,
+					important: !note.important,
+				})
+				set((state) => ({
+					notes: state.notes.map((n) => (n.id === id ? updated : n)),
+				}))
+			},
+			setFilter: (value) => set(() => ({ filter: value })),
+			initialize: async () => {
+				const notes = await noteService.getAllNotes()
+				set(() => ({ notes: notes }))
+			},
 		},
-		// toggleImportance: (id) =>
-		// 	set((state) => ({
-		// 		notes: state.notes.map((note) =>
-		// 			note.id === id ? { ...note, important: !note.important } : note,
-		// 		),
-		// 	})),
-		toggleImportance: async (id) => {
-			const note = get().notes.find((n) => n.id === id)
-			const updated = await noteService.update(id, {
-				...note,
-				important: !note.important,
-			})
-			set((state) => ({
-				notes: state.notes.map((n) => (n.id === id ? updated : n)),
-			}))
-		},
-		setFilter: (value) => set(() => ({ filter: value })),
-		initialize: async () => {
-			const notes = await noteService.getAllNotes()
-			set(() => ({ notes: notes }))
-		},
-	},
-}))
+	})),
+)
 
 export const useNotes = () => {
 	const notes = useNoteStore((state) => state.notes)
